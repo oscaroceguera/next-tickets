c67beb4bba2c55d293aba8a4e70853bfa733fcec
Oscar Oceguera bibriesca <oscaroceguera@gmail.com>
2024-11-12 18:30:38 -0700

    Init project

---
+++ app/details/[id]/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Detail() {
+  return (
+    <div className="border-4 border-red-700 h-screen">
+      <h1 className="text-red-700 border text-center p-4 text-xl">
+        DETALLE DEL TICKET
+      </h1>
+      <div className="border flex items-center justify-center gap-3">
+        <a href="/" className="border bg-purple-600 text-white p-2">
+          HOME PRINCIPAL
+        </a>
+        <Link href="/tickets" className="border bg-purple-600 text-white p-2">
+          HOME TICKET
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="border-4 border-red-700 h-screen">
+      <h1 className="text-red-700 border text-center p-4 text-xl">TICKETS</h1>
+      <div className="border flex items-center justify-center gap-3">
+        <a href="/" className="border bg-purple-600 text-white p-2">
+          HOME
+        </a>
+        <Link
+          href="/tickets/details/id"
+          className="border bg-purple-600 text-white p-2"
+        >
+          TICKET DETAIL
+        </Link>
+      </div>
+    </div>
+  );
+}
